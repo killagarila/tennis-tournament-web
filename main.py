25e93f12c7b46ac8dec7e14862bd11fd12e0304d
Ben Cummins <benjamin2.cummins@live.uwe.ac.uk>
2023-02-16 09:15:27 +0000

    Working flask app and updated file structure

---
+++ main.py
@@ -0,0 +1,29 @@
+#Main file used to run website
+#Imports needed for program
+from SD_code import *
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+import mysql.connector
+
+#Creating flask app
+app =Flask(__name__)
+app.secret_key='123' #Creating secret key
+#NEED TO ADD DATABASE FUNC HERE
+
+
+#Creating routes for website
+@app.route('/')
+def homepage():
+    print("homepage is open...")
+    return render_template('p1.html')
+@app.route('/newmatch')
+def newMatch():
+    print("new match is running")
+    return render_template('inputscreen.html')
+
+if __name__ == '__main__':
+    for i in range(13000, 18000):
+        try:
+            app.run(debug = True, port = i)
+            break
+        except OSError as e:
+            print("Port {i} not available".format(i))
