50c136bca07adf480854eaaf884414f1c042e075
e2-miliauskis <eimantas2.miliauskis@live.uwe.ac.uk>
2023-04-17 16:31:39 +0100

    AddMatch working

--- main.py
+++ main.py
@@ -69,62 +69,64 @@ def homepage():
     return render_template('p1.html',tournaments=tournaments, male_leaders=male_leaders, male_earners=male_earners, female_leaders=female_leaders, female_earners=female_earners)
 @app.route('/newmatch',methods=["GET","POST"])
 def newMatch():
-    tournaments=getTournaments()
-    return render_template('inputscreen.html',tournaments=tournaments)
+    if request.method == 'GET':
+        tournaments=getTournaments()
+        return render_template('inputscreen.html',tournaments=tournaments)
+    if request.method == 'POST':
 
-@app.route('/addmatch',methods=["GET","POST"])
-def newMatchForm():
-    tournaments=getTournaments()
-    print("new match is running")
-    session = DBSession()
-    #creating way to fetch input from form
-    #Variables from form
-    player1name=request.form['p1name']
-    player1score=request.form['p1score']
-    player2name=request.form['p2name']
-    player2score=request.form['p2score']
-    tournamentSel=request.form['tournsel']
-    tround=request.form['tround']
-    #print(tournamentSel)
-    
-    player1 = session.query(Players).filter_by(name=player1name)
-    player2 = session.query(Players).filter_by(name=player2name)
-    fktour = session.query(Tournaments).filter_by(name = tournamentSel)
+# @app.route('/addmatch',methods=["GET","POST"])
+# def newMatchForm():
+        tournaments=getTournaments()
+        print("new match is running")
+        session = DBSession()
+        #creating way to fetch input from form
+        #Variables from form
+        player1name=request.form['p1name']
+        player1score=request.form['p1score']
+        player2name=request.form['p2name']
+        player2score=request.form['p2score']
+        tournamentSel=request.form['tournsel']
+        tround=request.form['tround']
+        #print(tournamentSel)
+        
+        player1 = session.query(Players).filter_by(name=player1name).scalar()
+        player2 = session.query(Players).filter_by(name=player2name).scalar()
+        fktour = session.query(Tournaments).filter_by(name = tournamentSel).scalar()
+        print(fktour.tournament_id)
+        #Validating data inputaddmatch
+        if player1score==player2score:
+            error="Player scores cannot be the same"
+            return error,redirect(url_for('newMatch'))
+        if player1score > player2score:
+            winner=player1.player_id
+        elif player2score > player1score:
+            winner=player2.player_id
+        elif player1score==player2score:
+            error="Player scores cannot be the same"
+            return error,redirect(url_for('newMatch'))
+        #Validating score based on gender of tournament
+        if player1.gender == player2.gender:
+            if player1.gender == 'Male':
+                if int(player1score+player2score)<=5 or int(player1score+player2score)>=3:
+                    if player1score=="3" or player2score=="3":
+                        newMatch=Match(fkplayer_1=int(player1.player_id),fkplayer_2=int(player2.player_id),score_player1=int(player1score),score_player2=int(player2score),fkwinner=int(winner),fktournament=int(fktour.tournament_id),round=int(tround))
+                    else:
+                        return redirect(url_for('newMatch'))
+            elif player2.gender == 'Female':
+                if int(player1score+player2score)<=3 or int(player1score+player2score)>=2:
+                    if player1score=="2" or player2score=="2":
+                        newMatch=Match(fkplayer_1=int(player1.player_id),fkplayer_2=int(player2.player_id),score_player1=int(player1score),score_player2=int(player2score),fkwinner=int(winner),fktournament=int(fktour.tournament_id),round=int(tround))
+                    else:
+                        return error, redirect(url_for('newMatch'))
+        else:
+            error="Player genders must be the same"
+            return error, redirect(url_for('newMatch'),tournaments=tournaments)    
 
-    #Validating data input
-    if player1score==player2score:
-        error="Player scores cannot be the same"
-        return error,redirect(url_for('newmatch'))
-    if player1score > player2score:
-        winner=player1[0].player_id
-    elif player2score > player1score:
-        winner=player2[0].player_id
-    elif player1score==player2score:
-        error="Player scores cannot be the same"
-        return error,redirect(url_for('newmatch'))
-    #Validating score based on gender of tournament
 
-    if player1[0].gender == player2[0].gender:
-        if player1[0].gender == 'Male':
-            if int(player1score+player2score)<=5 or int(player1score+player2score)>=3:
-                if player1score==3 or player2score==3:    
-                    newMatch=Match(fkplayer_1=player1[0].player_id,fkplayer_2=player2[0].player_id,score_player1=player1score,score_player2=player2score,fkwinner=winner,fktournament=fktour[0].tournament_id,round=tround)
-                else:
-                    return error, redirect(url_for('newmatch'))
-        elif player2[0].gender == 'Female':
-            if int(player1score+player2score)<=3 or int(player1score+player2score)>=2:
-                if player1score==2 or player2score==2:
-                    newMatch=Match(fkplayer_1=player1[0].player_id,fkplayer_2=player2[0].player_id,score_player1=player1score,score_player2=player2score,fkwinner=winner,fktournament=fktour[0].tournament_id,round=tround)
-                else:
-                    return error, redirect(url_for('newmatch'))
-    else:
-        error="Player genders must be the same"
-        return error, redirect(url_for('newmatch'),tournaments=tournaments)    
-
-
-    #Creating object for match object
-    session.close()
-    return redirect(url_for('newmatch'))
+        #Creating object for match object
+        newMatch.commitToDB()
+        session.close()
+        return redirect(url_for('newMatch'))
 
 @app.route('/tournament',methods=["GET","POST"])
 def viewTournament():
